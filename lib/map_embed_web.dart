5383e0afac9b4cd7e98cac74ba2e2e64aa94f098
Abouerrahim Riahi <atlasshopy@gmail.com>
2026-09-23 19:22:53 +0200

    fix: use package web for Google Maps iframe

--- lib/map_embed_web.dart
+++ lib/map_embed_web.dart
@@ -1,7 +1,7 @@
-import 'dart:html' as html;
 import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/widgets.dart';
+import 'package:web/web.dart' as web;
 
 const _mapEmbedUrl = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2896.4525927437985!2d5.413858799999999!3d43.4511475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c993f6845e653f:0xb8b142df476df5ec!2sCentre%20de%20Luxopuncture%20Claudine%20Am%C3%A9rigo!5e0!3m2!1sfr!2sfr!4v1770476515356!5m2!1sfr!2sfr';
 bool _registered = false;
@@ -11,7 +11,7 @@ Widget buildGoogleMapEmbed() {
     ui_web.platformViewRegistry.registerViewFactory(
       'clauluxo-google-map',
       (int viewId) {
-        final frame = html.IFrameElement()
+        final frame = web.HTMLIFrameElement()
           ..src = _mapEmbedUrl
           ..style.border = '0'
           ..style.width = '100%'
