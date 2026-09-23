ba567ae80977ade19bd9a623097508bd6ad89864
Abouerrahim Riahi <atlasshopy@gmail.com>
2026-09-23 19:25:08 +0200

    test: scroll home before checking booking action

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:clauluxo_mobile/main.dart';
@@ -5,7 +6,15 @@ import 'package:clauluxo_mobile/main.dart';
 void main() {
   testWidgets('ClauLuxo home page renders', (tester) async {
     await tester.pumpWidget(const ClauLuxoApp());
+    await tester.pumpAndSettle();
+
     expect(find.text('Centre de Luxopuncture'), findsOneWidget);
+    expect(find.byType(OfficialHeroGallery), findsOneWidget);
+
+    final scrollable = find.byType(ListView).first;
+    await tester.drag(scrollable, const Offset(0, -600));
+    await tester.pumpAndSettle();
+
     expect(find.text('Planifier une séance'), findsOneWidget);
   });
 }
